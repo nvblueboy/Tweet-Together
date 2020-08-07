ea4215425c97cfc3a4ff0f8d70d3ed9708e1d5d4
Dylan Bowman <dbowman06@gmail.com>
2020-08-07 08:46:11 -0700

    Adds twitter support, censoring, queueing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const config = require("./config.json");
 const Discord = require("discord.js");
+const Twit = require("twit");
 const fs = require("fs");
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 
@@ -50,12 +51,143 @@ function storeMessageTweeted(guildId, messageId) {
     fs.writeFileSync(config.datafile, JSON.stringify(parsed));
 }
 
-//Discord Client info
+
+function formatDate(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+  }
+
+//Queueing functionality
+function checkQueue() {
+    var queue = readQueue();
+
+    var currentTime = new Date();
+    
+    for (var message of queue.messages) {
+        if (message.post_time < currentTime && !message.posted && !message.blocked) {
+            tweet(message.content);
+
+            message.posted = true;
+        }
+    }
+
+    fs.writeFileSync(config.queuefile, JSON.stringify(queue));
+}
+
+
+var queueInterval = setInterval(checkQueue, 1000);
+
+function readQueue() {
+    var parsed = {"messages":[]}
+
+    if (fs.existsSync(config.queuefile)) {
+        let rawdata = fs.readFileSync(config.queuefile);
+        parsed = JSON.parse(rawdata);
+    }
+
+    return parsed;
+}
+
+function addToQueue(message, outboundMessage) {
+    // console.log(outboundMessage);
+    var queue = readQueue();
+
+    var t = new Date();
+
+    var newMessage = {
+        "content" : message,
+        "post_time" : t.setSeconds(t.getSeconds() + config.waitperiod),
+        "posted" : false,
+        "blocked" : false,
+        "stopId" : outboundMessage.id,
+        "stopChannelId" : outboundMessage.channel.id,
+        "stopGuildId" : outboundMessage.channel.guild.id
+    };
+
+    queue.messages.push(newMessage);
+
+    fs.writeFileSync(config.queuefile, JSON.stringify(queue));
+}
+
+function isStopMessage(messageId) {
+    var queue = readQueue();
+
+    for (var message of queue.messages) {
+        if (message.stopId == messageId) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function blockTweet(stopMessageId) {
+    var queue = readQueue();
+
+    for (var message of queue.messages) {
+        if (message.stopId == stopMessageId) {
+            message.blocked = true;
+
+            notifyOfBlock(message);
+        }
+    }
+
+    fs.writeFileSync(config.queuefile, JSON.stringify(queue));
+}
+
+function notifyOfBlock(message) {
+    client.channels.fetch(message.stopChannelId).then(channel => {
+        channel.messages.fetch(message.stopId).then(message => {
+            var content = message.content;
+            message.edit("***Blocked, not Tweeting.***\n" + content);
+        }).catch(err => {
+            console.log(err);
+        })
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+//censoring
+function censorMessage(content) { 
+    //create a string for a regex to read from the config data
+    var searchText = config.censored_words.join("|");
+    var regEx = new RegExp(searchText, "ig");
+
+    return content.replace(regEx, "█████");
+}
+
+//Twitter functionality
+var T = new Twit({
+    consumer_key: config.twitter.api_key,
+    consumer_secret: config.twitter.api_secret_key,
+    access_token: config.twitter.access_token,
+    access_token_secret : config.twitter.access_token_secret,
+    timeout_ms: 60*1000
+});
+
+function tweet(message) { 
+    T.post('statuses/update', { status: message }, function(err, data, response) {
+        console.log(data);
+    })
+}
+
+
+
+//Discord functionality
 client.once("ready", () => {
     console.log("Connected to Discord");
 });
 
+
 client.on('messageReactionAdd', async (reaction, user) => {
+    //If the data about this reaction is not loaded, fetch the information before continuing.
     if (reaction.partial) {
         try {
             await reaction.fetch();
@@ -65,22 +197,46 @@ client.on('messageReactionAdd', async (reaction, user) => {
         }
     }
 
+    var message = reaction.message;
+
+
+    //If it's a bot's message, don't tweet it out. If it's a stop message, stop that tweet from happening.
+    if (message.author.bot) {
+        if (isStopMessage(message.id)) {
+            //It's a stop message. Make sure it's the "Stop" reaction and veto the release.
+            if (reaction.count > 1 && reaction.emoji.name==config.stopemoji) {
+                blockTweet(message.id);
+            }
+        }
+
+        return;
+    }
+
     if (reaction.count >= config.reaction_count) {
-        console.log("Message hit threshold.");
-
-        var message = reaction.message;
-
+        
         if (!wasMessageTweeted(message.channel.guild.id, message.id)) {
             //Not tweeted already. Go to town.
-            console.log("That's a new one");
+            var content = censorMessage(message.content);
 
-            console.log("Tweeting: "+message.content);
+            console.log("Preparing to tweet:")
+            console.log(content);
 
-            var outboundMessage = (await message.channel.send("Hit reaction limit, tweeting: " + message.content));
+            if (content.length > 240) {
+                message.channel.send("Message too long to tweet: " + content);
+                return;
+            }
 
-            console.log(outboundMessage);
+            var postDate = new Date();
+            postDate.setSeconds(postDate.getSeconds() + config.waitperiod);
+
+
+            var outboundMessage = (await message.channel.send("Popular message you got there. Tweeting at " + formatDate(postDate) + "\n> " + content + "\nReact with "+config.stopemoji+" to block this from being tweeted."));
+
+            outboundMessage.react(config.stopemoji);
 
             storeMessageTweeted(message.channel.guild.id, message.id);
+
+            addToQueue(content, outboundMessage);
         } else {
             console.log("Already tweeted, moving along.");
         }
